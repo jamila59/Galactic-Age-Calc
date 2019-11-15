3bd718a38440ec97d37c6776145c337b938eb357
Jamila Aliyeva <padma@email.com>
2019-11-15 14:37:07 -0800

    be working on displaying output to user and giving page a background

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
-import { Galactic, mercuryAge} from './back-end';
+import { Galactic} from './back-end';
 
 
 
@@ -10,14 +10,11 @@ $(document).ready(function() {
   $("form#conversionForm").submit(function(event) {
     event.preventDefault();
     let userAge = parseInt($("input#userAge").val());
+    debugger;
     let input = new Galactic(userAge);
-    input.mercuryChecker();
-    input.venusChecker();
-    input.marsChecker();
-    input.jupiterChecker();
-    (".hiddenMercury").text(mercuryAge);
-    // (".hiddenVenus").text
-    // (".hiddenMars").text
-    // (".hiddenJupiter").text
+    (".hiddenMercury").text(input.mercuryChecker());
+    (".hiddenVenus").text(input.venusChecker());
+    (".hiddenMars").text(input.marsChecker());
+    (".hiddenJupiter").text(input.jupiterChecker());
   });
 });
