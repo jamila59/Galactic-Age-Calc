7d7c19e15c9949cd3d7511cfeb3664abb03ab840
Jamila Aliyeva <padma@email.com>
2019-11-15 07:59:32 -0800

    clear up template for new project

--- src/main.js
+++ src/main.js
@@ -2,14 +2,11 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
-import { convert } from './roman-numerals';
+import {  } from './back-end';
 
 
 $(document).ready(function() {
   $("form#conversionForm").submit(function(event) {
     event.preventDefault();
-    var conversion = convert($("input#userInput").val());
-    $("#conversionResult").text(conversion);
-    $("#result").show();
   });
 });
